b6252aecfdd3cce8441efe0829e36b452cac23d2
Cameron E Baird <t-cbaird@microsoft.com>
2020-07-30 18:51:47 -0700

    Namespaced sequences sync (#4)
    * add return values
    
    * (broken) in progress nested type sequences
    
    * Add support for sequences of namespaced types
    
    * Expose TransformListener interface from tf2 to C#
    
    * C# portion of tf interface
    
    * clean up interface and document
    
    Co-authored-by: Lou Amadio <lamadio@microsoft.com>

---
+++ rclcppdotnet/rclcppdotnet.h
@@ -0,0 +1,15 @@
+#ifndef RCL_DOTNET_H
+#define RCL_DOTNET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	__declspec(dllexport)
+	void __cdecl native_rclcpp_init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // RCL_DOTNET_H
