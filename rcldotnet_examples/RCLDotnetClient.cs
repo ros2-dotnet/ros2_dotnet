cee0f2ebfaad7c606b90d974fa7b5a319be81e34
Stefan Hoffmann <stefan.hoffmann@schiller.de>
2021-08-31 15:34:15 +0200

    Add client example

---
+++ rcldotnet_examples/RCLDotnetClient.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Reflection;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using ROS2;
+
+namespace ConsoleApplication
+{
+    public static class RCLDotnetClient
+    {
+        public static void Main(string[] args)
+        {
+            RCLdotnet.Init();
+            var node = RCLdotnet.CreateNode("client");
+
+            var client = node.CreateClient<std_srvs.srv.SetBool, std_srvs.srv.SetBool_Request, std_srvs.srv.SetBool_Response>("test_dotnet_service_name");
+            
+            // TODO: (sh) WaitForService(timeout)
+            Thread.Sleep(2000);
+            
+            var request = new std_srvs.srv.SetBool_Request();
+            request.Data = true;
+
+            var task = client.SendRequestAsync(request);
+
+            Console.WriteLine("Sent request.");
+            
+            while (RCLdotnet.Ok())
+            {
+                RCLdotnet.SpinOnce(node, 500);
+
+                if (task.IsCompletedSuccessfully)
+                {
+                    var response = task.Result;
+                    Console.WriteLine($"Service responded with: Success '{response.Success}' Message '{response.Message}'");
+                    break;
+                }
+                else if (task.IsFaulted)
+                {
+                    Console.WriteLine($"Task faulted. Exception {task.Exception}");
+                    break;
+                }
+                else if (task.IsCanceled)
+                {
+                    Console.WriteLine("Task canceld.");
+                    break;
+                }
+            }
+        }
+    }
+}
